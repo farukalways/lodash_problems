881754314f5588d5a2a50071d98e58b0af706363
farukalways <alishopon367@gmail.com>
2026-06-04 08:07:08 +0600

    problem 03 resolve

--- problem_03.js
+++ problem_03.js
@@ -8,13 +8,13 @@
 // console.log(array);
 // => [1]
 
-const arr1 = [1, 3];
+const arr1 = 3;
 
 const arr2 = [2, [4], 67];
 const arr3 = 5;
 
 function concatArr(arr1, ...arr2) {
-  let result = [...arr1];
+  let result = Array.isArray(arr1) ? [...arr1] : [arr1];
 
   for (let i = 0; i < arr2.length; i++) {
     if (Array.isArray(arr2[i])) {
